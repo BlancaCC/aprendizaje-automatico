bedd61d95e11f33b673870b8d597e4b83d3f2b70
Blanca <blancacano2.71828@gmail.com>
2021-03-08 09:44:52 +0100

    [practica 0] corrijo función y añado test

--- practica0/practica0_2.py
+++ practica0/practica0_2.py
@@ -25,16 +25,9 @@ numero_clases = len(iris.target_names)
 X = iris.data # obtenemos los datos
 y = iris.target # obtenemos las clases
 
-len_target = len(y/ numero_clases)
 
-datos_por_clases = [X[ i*len_target:(i+1)*len_target] for i in range(numero_clases) ]
-
-[ print( i) for i in datos_por_clases]
-
-
-""" DE ESTA MANERA LA DIVISIÓN NO ES PERFECTA 
 test_size = 0.25
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, shuffle = True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, shuffle = True, stratify=y)
 
 print( 'Datos de entrenamiento: \n' , X_train)
 print( 'Target de entrenamiento: \n ', y_train)
@@ -42,7 +35,10 @@ print( 'Target de entrenamiento: \n ', y_train)
 print( 'Datos de test: \n' , X_test)
 print( 'Target de test: \n ', y_test)
 
-"""
 ## Comprobación de que la partición está bien hecha
 
+for i in range( len (iris.feature_names)):
+    print( 'numero de datos para test de clase ', i )
+    print ( sum (  [ 1 if i == i else 0 for i in y_test]))
 
+print( 'Hemos observado que hay la mismas cantidad de datos luego la separación es correcta ')
