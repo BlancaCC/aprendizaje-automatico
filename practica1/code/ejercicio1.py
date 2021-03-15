51c7ee0664dbac6e77f359ee2ea189ecd053df64
Blanca <blancacano2.71828@gmail.com>
2021-03-15 14:29:52 +0100

    Introducciendo derivadas y gradiente

--- practica1/code/ejercicio1.py
+++ practica1/code/ejercicio1.py
@@ -13,65 +13,85 @@ np.random.seed(1)
 print('EJERCICIO SOBRE LA BUSQUEDA ITERATIVA DE OPTIMOS\n')
 print('Ejercicio 1\n')
 
-def E(u,v):
-    ''' (ESTO NO TIENE PORQUÉ SE  ASÍ)
-    (vector, vector) -> real positivo 
-    Error cuadrático medio
-    '''
+## 1
 
-    
-    return #function   
-
-#Derivada parcial de E con respecto a u
-def dEu(u,v):
-    return #Derivada parcial de E con respecto a u
-    
-#Derivada parcial de E con respecto a v
-def dEv(u,v):
-    return #Derivada parcial de E con respecto a v
-
-#Gradiente de E
-def gradE(u,v):
-    return np.array([dEu(u,v), dEv(u,v)])
-
-def gradient_descent(inicial_point, E, gradient_function,  eta=0.1):
+def gradient_descent(initial_point, E, gradient_function,  eta, max_iter, max_error):
     '''
     initicial point: w_0 
     E: error function 
     gradient_function
     eta:  step size 
+
+    ### stop conditions 
+    max_iter
+    max_error
     '''
-    #### stop conditions   ######
-    max_iter = 1000
-    max_error =  0.0001
-    ####
 
     iterations = 0
-    current_error = E( intial_point[0], intial_point[1])
+    error = E( initial_point[0], initial_point[1])
     w = initial_point
-
-    while iterations < max_iter and error > max_error:
+  
+    while ( (iterations < max_iter) and(error > max_error)): 
 
         w = w - eta * gradient_function(w[0], w[1])
+        
         iterations += 1
-
+        error = E(w[0], w[1])
+        print(w)
+ 
     
+    return w, iterations
 
+## 2
+def E(u,v):
+    '''
+    Function to minimize
+    '''
+    return np.float64(
+        (
+            u**3 * np.e**(v-2) - 2*v**2 * np.e**(-u)
+        )**2
+    )
+
+
+def dEu(u,v):
+    '''
+    Partial derivate of E with respect to the variable u
+    '''
+    return np.float64(
+        2*(
+           3* u**2 * np.e**(v-2) + 2*v**2 * np.e**(-u)
+        )
+    )
     
-    return w, iterations    
+def dEv(u,v):
+    '''
+    Partial derivate of E with respect to the variable v
+    '''
+    return np.float64(
+        2*(
+        u**3 * np.e**(v-2) - 4*v * np.e**(-u)
+        )
+    )
 
+
+def gradE(u,v):
+    ''' 
+        gradient of E
+    '''
+    return np.array([dEu(u,v), dEv(u,v)])
 
 eta = 0.01 
-maxIter = 10000000000
-error2get = 1e-14
+max_iter = 100#00000000
+error_to_get = 1e-14
 initial_point = np.array([1.0,1.0])
-w, it = gradient_descent(?)
+w, it = gradient_descent( initial_point,E, gradE, eta, max_iter, error_to_get )
 
 
 print ('Numero de iteraciones: ', it)
 print ('Coordenadas obtenidas: (', w[0], ', ', w[1],')')
 
-
+'''
 # DISPLAY FIGURE
 from mpl_toolkits.mplot3d import Axes3D
 x = np.linspace(-30, 30, 50)
@@ -92,3 +112,4 @@ ax.set_zlabel('E(u,v)')
 
 input("\n--- Pulsar tecla para continuar ---\n")
 
+'''
